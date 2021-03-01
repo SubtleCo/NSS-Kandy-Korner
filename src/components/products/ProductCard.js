ea8c3352b733b81606600c624f6e28f0511d71eb
Alex Martin <alex@subtleco.co>
2021-03-01 15:00:02 -0600

    created ProductTypeProvider, now displaying ProductType.name on products list

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const ProductCard = ({ product }) => {
+export const ProductCard = ({ product, productType }) => {
 
     return (
         <article className="product">
             <h2 className="product__name">{product.name}</h2>
             <p className="product__id">Product # {product.id}</p>
             <p className="product__price">Cost: ${product.price}</p>
-            <p className="product__typeId">ProductTypeId: {product.productTypeId}</p>
+            <p className="product__type">Product Type: {productType.name}</p>
 
         </article>
     )
